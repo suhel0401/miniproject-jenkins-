ba15c4f0ca2cb70ed6312644ba0435488d533d91
suhel0401 <suhelfahi@gmail.com>
2025-10-22 17:16:21 +0530

    Create App.java

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,7 @@
+package com.loopwear;
+
+public class LoopwearApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Loopwear — Where Code Meets Style!");
+    }
+}
